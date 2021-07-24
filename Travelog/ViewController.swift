950589225540e1a9d1c86f8b06152dbf1e80c4f4
elppaaa <withbcd@gmail.com>
2021-07-24 20:13:17 +0900

    chore: Delete storyboard

--- Travelog/ViewController.swift
+++ Travelog/ViewController.swift
@@ -12,8 +12,17 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    let text = UILabel()
+    text.text = "Hello world"
+    text.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(text)
+    view.backgroundColor = .systemBackground
+    NSLayoutConstraint.activate([
+      text.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      text.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      text.widthAnchor.constraint(greaterThanOrEqualToConstant: 30)
+    ])
   }
-
 
 }
 
