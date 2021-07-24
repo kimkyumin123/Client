0af45c51fb831a8290a639c2c7b1b30900d867c2
elppaaa <withbcd@gmail.com>
2021-07-24 20:08:31 +0900

    Initial Commit

---
+++ Travelog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travelog
+//
+//  Created by JK on 2021/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
