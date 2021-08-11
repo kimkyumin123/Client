2a43f0ea4acbd20031116132c1b0202afeb71423
elppaaa <withbcd@gmail.com>
2021-08-12 01:17:43 +0900

    feat: Add Logger, Keychain Manager
    - Logging 을 위한 카테고리 추가
    - Keychain 을 읽고 쓸 때 사용할 매니저 추가

---
+++ Travelog/Utils/UserManager.swift
@@ -0,0 +1,28 @@
+//
+//  UserManager.swift
+//  Travelog
+//
+//  Created by JK on 2021/08/11.
+//
+
+import Foundation
+
+final class UserManager {
+  static func createUser() { }
+
+  static func createOAuthUser() { }
+
+  static func checkValidate(nickname: String) { }
+
+  static func checkValidate(email: String) { }
+
+  static func updateUser() { }
+
+  static func deleteUser() { }
+
+  static func login() { }
+
+  static func oAuthLogin() { }
+
+  static func logout() {}
+}
